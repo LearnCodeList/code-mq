9f3c6b2034f187e406ecaa334c90353a6c349826
weixing.yang <weixingmail@126.com>
2021-07-14 15:55:35 +0800

    commit 'test'

--- rocketmq/src/main/java/cn/code/constant/ConfigConstant.java
+++ rocketmq/src/main/java/cn/code/constant/ConfigConstant.java
@@ -2,6 +2,6 @@ package cn.code.constant;
 
 public class ConfigConstant {
 
-    public final static String RQ_NAMESRV_ADDR = "127.0.0.1:9876";
+    public final static String RQ_NAMESRV_ADDR = "192.168.115.226:9876";
 
 }
